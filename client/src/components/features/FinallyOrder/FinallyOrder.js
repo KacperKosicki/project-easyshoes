4263a2dc9eef0095122fac0c140fe1f9ab7e0021
Kacper Kosicki <kosickikacper1@gmail.com>
2024-01-31 16:45:28 +0100

    Modal fix in FinallyOrder

--- client/src/components/features/FinallyOrder/FinallyOrder.js
+++ client/src/components/features/FinallyOrder/FinallyOrder.js
@@ -80,9 +80,9 @@ const FinallyOrder = () => {
         <div className="modal" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
           <div className="modal-dialog" role="document">
             <div className="modal-content">
-              <div className="modal-header">
+              <div className={`modal-header ${styles.customHeader}`}>
                 <h5 className="modal-title">Potwierdzenie zamówienia</h5>
-                <button type="button" className="close" onClick={handleModalClose} aria-label="Close">
+                <button type="button" className={`close ${styles.customClose}`} onClick={handleModalClose} aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
@@ -91,7 +91,7 @@ const FinallyOrder = () => {
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" onClick={handleModalClose}>Anuluj</button>
-                <button type="button" className="btn btn-primary" onClick={sendOrderToServer}>Złóż zamówienie</button>
+                <button type="button" className="btn btn-success" onClick={sendOrderToServer}>Złóż zamówienie</button>
               </div>
             </div>
           </div>
