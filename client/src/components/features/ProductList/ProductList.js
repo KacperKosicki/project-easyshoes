c77a7c8dc258bceb51a1290e41630c4f18a1d330
Kacper Kosicki <kosickikacper1@gmail.com>
2024-03-05 12:42:15 +0100

    Return to working code on render

--- client/src/components/features/ProductList/ProductList.js
+++ client/src/components/features/ProductList/ProductList.js
@@ -36,8 +36,7 @@ const ProductList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apiUrl = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_API_URL : process.env.REACT_APP_RENDER_API_URL;
-        const response = await axios.get(apiUrl);
+        const response = await axios.get('/api/products'); // Zmiana adresu URL
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
